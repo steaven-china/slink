a847507eeaf54c2fc21f3fba404a913f7664d237
Steaven Jiang <rgterff@163.com>
2026-05-08 19:57:15 +0800

    ci: add build workflow and nuitka wrappers

---
+++ build_sli.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+"""Wrapper for Nuitka to build sli CLI."""
+from slink.cli import main
+main()
---
+++ build_sli_ui.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+"""Wrapper for Nuitka to build sli-ui GUI."""
+from slink.gui import main
+main()
